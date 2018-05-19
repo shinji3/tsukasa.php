15b78bde6aae2bc3d52da4d372a3ab28606d76b6
shinji3 <renrakusaki2003@yahoo.co.jp>
2018-05-19 20:42:04 +0900

    最初のコミット

---
+++ WindowsFormsApp1/WindowsFormsApp1/Form1.cs
@@ -0,0 +1,41 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+using System.Diagnostics;
+using System.Xml.Linq;
+using System.Xml.XPath;
+
+namespace WindowsFormsApp1
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        private void button1_Click(object sender, EventArgs e)
+        {
+            var uri = new UriBuilder(textBox1.Text);
+            uri.Scheme = "mmsh";
+            var proc = new Process();
+            proc.StartInfo.FileName = "ffprobe";
+            proc.StartInfo.Arguments = $"-loglevel quiet -show_streams -print_format xml -i {uri.ToString()}";
+            proc.StartInfo.UseShellExecute = false;
+            proc.StartInfo.RedirectStandardOutput = true;
+            proc.Start();
+            var xml = XDocument.Parse(proc.StandardOutput.ReadToEnd());
+            var stream = xml.XPathSelectElement("//stream");
+            var start_pts = stream.Attribute("start_pts");
+            var start_time = (DateTimeOffset.UtcNow.ToUnixTimeMilliseconds() - int.Parse(start_pts.Value) - 12000) / 1000;
+            uri.Scheme = "httpt";
+            textBox2.Text = $"?start_time={start_time}&ref={Uri.EscapeDataString(uri.ToString())}";
+        }
+    }
+}
