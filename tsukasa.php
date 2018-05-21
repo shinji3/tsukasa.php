82c1aab9b3c6528e0d5a666d9fb96e79eddcf98b
shinji3 <renrakusaki2003@yahoo.co.jp>
2018-05-21 11:09:47 +0900

    配信時間が24時間以上の時に配信時間を正常に取得出来ない不具合を修正しました

--- tsukasa.php
+++ tsukasa.php
@@ -2,9 +2,10 @@
 header("Content-Type: video/x-ms-asf");
 header("Cache-Control: no-cache");
 date_default_timezone_set("UTC");
+$start_time = isset($_GET["start_time"]) ? (time() - (int)$_GET["start_time"]) : 0;
 ?><ASX version="3.0">
 <Entry>
-<StartTime Value="<?php echo date("H:i:s", isset($_GET["start_time"]) ? (time() - (int)$_GET["start_time"]) : 0); ?>" />
+<StartTime Value="<?php echo (string)floor($start_time / 3600) . date(":i:s", $start_time); ?>" />
 <Ref href="<?php echo isset($_GET["ref"]) ? htmlspecialchars($_GET["ref"], ENT_QUOTES) : ""; ?>" />
 </Entry>
 </ASX>
